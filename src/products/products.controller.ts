55876c590ee3f47ecff05724764d3ef28e70e128
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-09-15 16:08:58 +0200

    add products and orders

---
+++ src/products/products.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Delete, Get, Param, Post, Body } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDTO } from './dtos/create-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  public deleteById(@Param('id') id: string) {
+    this.productsService.deleteById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  public create(@Body() productData: CreateProductDTO) {
+    return this.productsService.create(productData);
+  }
+}
